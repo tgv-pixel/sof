3ada719089eba488c65f2557b513e67cb72fdfcc
tgv-pixel <noelcan515@gmail.com>
2026-05-10 15:21:47 +0300

    Update server.py

--- server.py
+++ server.py
@@ -35,7 +35,7 @@ CORS(app)
 # ============================================
 # CHANGE THIS NUMBER ONLY - 1 TO 5
 # ============================================
-SERVER_NUMBER = 4  # 1=Dil, 2=sofu, 3=bebby, 4=kaleb, 5=fitsum
+SERVER_NUMBER = 2  # 1=Dil, 2=sofu, 3=bebby, 4=kaleb, 5=fitsum
 
 SERVERS = {
     1: {'name': 'Dil', 'api_id': 35790598, 'api_hash': 'fa9f62d821f04b03d76d53175e367736', 'url': 'https://dilbedl.onrender.com'},
